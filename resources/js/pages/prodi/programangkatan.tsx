eb3a0de2c4bb6d54fec8881fc671c50bfc4209f3
Khaeril Maswal Zaid <68152480+khaeril-maswal-zaid@users.noreply.github.com>
2025-03-22 13:35:11 +0800

    make view program angkatan

--- resources/js/pages/prodi/programangkatan.tsx
+++ resources/js/pages/prodi/programangkatan.tsx
@@ -6,7 +6,7 @@ import { Head } from '@inertiajs/react';
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Program Angkatan',
-        href: '/dashboard-prodi',
+        href: '/program-angkatan',
     },
 ];
 
@@ -15,12 +15,22 @@ export default function programAngkatan() {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div className="grid auto-rows-min gap-4 md:grid-cols-2">
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                    <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                        <table className="w-full border-collapse border border-gray-300">
+                            <thead>
+                                <tr className="bg-gray-100">
+                                    <th className="border border-gray-300 px-4 py-1.5 text-xs">No</th>
+                                    <th className="border border-gray-300 px-4 py-1.5 text-xs">Angkatan</th>
+                                    <th className="border border-gray-300 px-4 py-1.5 text-xs">Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <td className="border border-gray-300 px-2.5 py-1.5 text-center text-xs text-gray-700 dark:text-gray-600"></td>
+                                <td className="border border-gray-300 px-2.5 py-1.5 text-center text-xs text-gray-700 dark:text-gray-600"></td>
+                                <td className="border border-gray-300 px-2.5 py-1.5 text-center text-xs text-gray-700 dark:text-gray-600"></td>
+                            </tbody>
+                        </table>
                     </div>
                     <div className="border-sidebar-border/70 dark:border-sidebar-border relative aspect-video overflow-hidden rounded-xl border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
