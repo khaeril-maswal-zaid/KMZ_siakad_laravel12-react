f19bacebf9a07dfdb66ca00b8a8b08b64d17feba
Khaeril Maswal Zaid <muhammadkhaerilzaid@gmail.com>
2025-03-29 00:54:10 +0800

    Buat filer mahasiswa

--- app/Http/Controllers/MahaiswaUserController.php
+++ app/Http/Controllers/MahaiswaUserController.php
@@ -11,20 +11,35 @@ use Illuminate\Support\Facades\Auth;
 class MahaiswaUserController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
         // Ambil user yang sedang login
         $user = Auth::user();
 
         // Ambil program_studi_id dari Admin yang sedang Login
         $prodiIdAdminLogin = $user->adminProdi->program_studi_id;
+        $query = MahasiswaUser::with('user:id,name,email')
+            ->select(['nim', 'angkatan', 'kelas', 'jenis_kelamin', 'tempat_lahir', 'tanggal_lahir', 'no_hp', 'user_id'])
+            ->where(function ($query) use ($request) {
+                $query->whereHas('user', function ($q) use ($request) {
+                    $q->where('name', 'LIKE', "%{$request->abboya}%");
+                })
+                    ->orWhere('nik', 'LIKE', "%{$request->abboya}%")
+                    ->orWhere('nim', 'LIKE', "%{$request->abboya}%");
+            });
+
+        // Terapkan filter program studi jika diperlukan
+        $query->where('program_studi_id', $prodiIdAdminLogin);
+
+        // Hanya tambahkan filter angkatan jika parameter tersebut ada
+        if ($request->has('angkatan')) {
+            $query->where('angkatan', $request->angkatan);
+        }
 
         $data = [
-            'mahasiswas' => MahasiswaUser::with('user:id,name,email')
-                ->select(['nim', 'kelas', 'jenis_kelamin', 'tempat_lahir', 'tanggal_lahir', 'no_hp', 'user_id'])
-                ->where('program_studi_id', $prodiIdAdminLogin)->latest()
-                ->paginate(15)
+            'mahasiswas' => $query->orderBy('nim', 'asc')->paginate(15)
         ];
+
 
         return Inertia::render('prodi/mahasiswa', $data);
     }
